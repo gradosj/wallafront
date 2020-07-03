2e95bfaec85c4c2f28d452ddad9e9bc51b0fded9
jose antonio grados barriga <joseantoniogradosbarriga@iMac-de-jose.local>
2020-07-03 11:55:12 +0200

    añadimos listados de anuncios

--- src/components/anuncios/ListadoAnuncios.js
+++ src/components/anuncios/ListadoAnuncios.js
@@ -5,21 +5,21 @@ const ListadoAnuncios = () => {
 
     const anuncios = [
         {nombre: 'Nombre anuncio', estado: true},
-        {nombre: 'Nombre anuncio1', estado: true},
+        {nombre: 'Nombre anuncio1', estado: false},
         {nombre: 'Nombre anuncio2', estado: true},
-        {nombre: 'Nombre anuncio3', estado: true},
+        
     ];
   return (
     <Fragment>
       <h2>Proyectos: Tienda virtual</h2>
 
-      <ul className="listado-anuncios">
+      <ul className="listado-tarea">
           {anuncios.length === 0
             ? (<li className="misAnuncios"><p>No hay anuncios</p></li>) 
-            : anuncios.map(misAnuncios => (
-                <MisAnuncios misAnuncios={anuncios}>
+            : anuncios.map(anuncios => (
+                <MisAnuncios misAnuncios={anuncios}/>
                     
-                </MisAnuncios>
+                
             )) 
           }
       </ul>
--- src/components/anuncios/MisAnuncios.js
+++ src/components/anuncios/MisAnuncios.js
@@ -1,7 +1,54 @@
 import React from 'react'
 
-const MisAnuncios = () => {
-    return (<p> desde tarea</p>);
+
+/* Desde tareas  TAREA*/ 
+
+const MisAnuncios = ({misAnuncios}) => {
+    console.log('desde Mis anuncios');
+    console.log(misAnuncios);
+    return (
+    <li className="tarea sombra">
+        
+        <p>{misAnuncios.nombre}</p>
+        <div className="estado">
+            {misAnuncios.estado
+            ?
+                (
+                    <button
+                        type="buttom"
+                        className="completo"
+                        >Completo</button>
+
+             )
+            :
+                (
+                    <button
+                        type="buttom"
+                        className="incompleto"
+                        >Incompleto</button>
+
+            )
+            
+            }
+        </div>
+        <div className="acciones">
+            <button
+                type="button"
+                className="btn btn-primario">
+                Editar
+            </button>
+
+            <button
+            type="button"
+            className="btn btn-secundario">
+            
+                Eliminar
+            </button>
+
+        </div>
+    </li>
+    );
+
 }
 
 export default MisAnuncios;
