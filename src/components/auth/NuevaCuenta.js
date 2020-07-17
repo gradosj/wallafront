b36ae22aa4c8c6e01822938688d8e063afa2aedf
jose antonio grados barriga <joseantoniogradosbarriga@iMac-de-jose.local>
2020-07-17 10:38:44 +0200

    funciona el registro de usuarios

--- src/components/auth/NuevaCuenta.js
+++ src/components/auth/NuevaCuenta.js
@@ -15,7 +15,7 @@ const NuevaCuenta = (props) => {
     // En caso de que el usuario se haya autenticado o registrado o sea un registro duplicado
     useEffect(() => {
         if(autenticado) {
-            props.history.push('/anuncios');
+            props.history.push('/privado');
         }
 
         if(mensaje) {
